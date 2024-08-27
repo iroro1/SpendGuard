8304bbe2d56dc820845f3fb75e161a5bdfe482b1
iroro1 <ojigboleo@gmail.com>
2024-08-27 10:45:11 +0100

    tests

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,20 @@
 "use client";
 import DashComp from "@/components/DashComp";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 import { Suspense } from "react";
 
 const Dashboard = () => {
   return (
-    <div className="min-h-screen bg-gray-100 py-10 px-6">
-      <Suspense fallback={<div>Loading...</div>}>
-        <DashComp />
-      </Suspense>
-    </div>
+    <>
+      <Header />
+      <div className="min-h-screen pt-[100px] bg-gray-100 py-10 px-6">
+        <Suspense fallback={<div>Loading...</div>}>
+          <DashComp />
+        </Suspense>
+      </div>
+      <Footer />
+    </>
   );
 };
 
